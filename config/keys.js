998cee561cca5234edef19ba14792653a5dc1e89
Mohammad Hasan <mohammadhasan@Mohammads-MacBook-Air.local>
2019-03-17 05:55:35 +0530

    Changed the key

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI : `mongodb+srv://hasan:hasan@devconnector-j1to3.mongodb.net/test?retryWrites=true`
+    mongoURI : `mongodb+srv://hasan:hasan@cluster0-xtcih.mongodb.net/test?retryWrites=true`
 }
